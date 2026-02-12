19c1ab2074e342d5aff837bc2cb42dd585a37270
Ecaf-Dev <eldestinegames@gmail.com>
2026-02-12 18:37:06 -0300

    Adicionado o stack de itens
    Adicionado a função que faz o stack de itens ocorrer e preparado a função diminuir stack

--- Scripts/rigid_body_3d_objeto.gd
+++ Scripts/rigid_body_3d_objeto.gd
@@ -1,6 +1,7 @@
 extends RigidBody3D
 
 @export var nome_item : String = ""
+@export var quantidade_atual : int = 1
 
 # Chamamos no _ready para que o visual mude assim que o item nascer no mundo
 func _ready():
@@ -8,15 +9,25 @@ func _ready():
 	_carregar_visual_automatico()
 
 func _carregar_visual_automatico():
+	
+	for child in get_children():
+		if child is MeshInstance3D or child is Node3D and child.name != "CSGBox3D_ObjetoVisual" and child.name != "CollisionShape3D" and child.name != "Area3D_Monitor":
+			# Se o nó não for um dos componentes básicos, é um modelo antigo
+			child.queue_free()
+	
 	# 1. Padroniza o nome para o formato de arquivo (ex: "Flor Da Vida" -> "flor_da_vida")
 	var nome_arquivo = nome_item.to_lower().replace(" ", "_")
 	print(nome_arquivo)
 	# 2. Define os caminhos possíveis (você pode adicionar .glb ou .tscn)
 	var caminho_modelo = "res://GirlsAndPotions/Modelos/" + nome_arquivo + ".tscn"
-	
+	var caminho_modelo_caixa = "res://GirlsAndPotions/Modelos/" + "caixa" + ".tscn"
 	# 3. Verifica se o arquivo realmente existe na pasta
 	if FileAccess.file_exists(caminho_modelo):
-		var cena_modelo = load(caminho_modelo)
+		var cena_modelo = null
+		if quantidade_atual == 1:
+			cena_modelo = load(caminho_modelo)
+		elif quantidade_atual  >= 2:
+			cena_modelo = load(caminho_modelo_caixa)			
 		if cena_modelo:
 			var instancia = cena_modelo.instantiate()
 			
@@ -37,3 +48,30 @@ func _carregar_visual_automatico():
 	else:
 		print("Aviso: Modelo para '", nome_item, "' não encontrado em ", caminho_modelo, ". Mantendo caixa rosa.")
 
+
+
+func _on_area_3d_monitor_body_entered(body):
+	if body.get("nome_item") == self.nome_item: # Replace with function body.
+		print("Itens iguais")
+		if "quantidade_atual" in body:
+			if body.freeze == false:
+				_stackaritens(body)
+
+func  _stackaritens(outro_item):
+	if outro_item.quantidade_atual > self.quantidade_atual :
+		print("meu grupo é menor")
+		_aumentarquantidade(outro_item)
+	elif outro_item.quantidade_atual == self.quantidade_atual:
+		if outro_item.global_position.y < self.global_position.y :
+			_aumentarquantidade(outro_item)
+
+func _aumentarquantidade(outro_item):
+	outro_item.quantidade_atual = outro_item.quantidade_atual + self.quantidade_atual
+	outro_item._carregar_visual_automatico()
+	queue_free()
+
+func _diminuirquantidade():
+	if(quantidade_atual != 1):
+		quantidade_atual = quantidade_atual -1
+	if(quantidade_atual == 1):
+		_carregar_visual_automatico()
