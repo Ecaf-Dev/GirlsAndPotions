485079045808dbeb2efa777d3c10c7ef2fa60e0c
Ecaf-Dev <eldestinegames@gmail.com>
2026-02-15 17:14:54 -0300

    Hud de troca de tela funcional!

--- Scripts/Global.gd
+++ Scripts/Global.gd
@@ -23,24 +23,3 @@ func alterar_prestigio(valor):
 	prestigio = prestigio + valor
 	prestigio_alterado.emit(prestigio)
 
-func comprar_recurso(item_path, preco, quantidade):
-	if moedas >= (preco * quantidade):
-		moedas -= (preco * quantidade)
-		moedas_alteradas.emit(moedas)
-		
-		# Faz o spawn para cada unidade comprada
-		for i in range(quantidade):
-			spawnar_item(item_path)
-		return true
-	return false
-
-func spawnar_item(path):
-	var item_cena = load(path)
-	var item_instancia = item_cena.instantiate()
-	
-	# Adiciona o item na cena principal (para não sumir se a caixa sumir)
-	get_tree().current_scene.add_child(item_instancia)
-	
-	# Define a posição para o Marker3D que você criou
-	var spawner = caixa_correio.get_node("Marker3D_Spawner")
-	item_instancia.global_position = spawner.global_position
--- Scripts/control_user_interface.gd
+++ Scripts/control_user_interface.gd
@@ -2,12 +2,15 @@ extends Control
 
 @onready var painel_livro = $CanvasLayer/Control_Painel_Livro
 @onready var label_moedas = $CanvasLayer/Control_HUD_Principal/HBoxContainer_Contador_Moedas/Label
-@onready var label_pedidosentregues = $"CanvasLayer/Control_Painel_Livro/MarginContainer/ColorRect/Control_Glossário/VBoxContainer_PaginaEsquerda/Label_PedidosEntregues"
-@onready var label_ourofaturado = $"CanvasLayer/Control_Painel_Livro/MarginContainer/ColorRect/Control_Glossário/VBoxContainer_PaginaEsquerda/Label_OuroFaturado"
-@onready var label_prestigio = $"CanvasLayer/Control_Painel_Livro/MarginContainer/ColorRect/Control_Glossário/VBoxContainer_PaginaEsquerda/Label_Prestigio"
+@onready var label_pedidosentregues = $"CanvasLayer/Control_Painel_Livro/MarginContainer/Control_Glossário/VBoxContainer_PaginaEsquerda/Label_PedidosEntregues"
+@onready var label_ourofaturado = $"CanvasLayer/Control_Painel_Livro/MarginContainer/Control_Glossário/VBoxContainer_PaginaEsquerda/Label_OuroFaturado"
+@onready var label_prestigio = $"CanvasLayer/Control_Painel_Livro/MarginContainer/Control_Glossário/VBoxContainer_PaginaEsquerda/Label_Prestigio"
 
 var livro_aberto = false
 var moedas_atuais = 0
+
+@onready var container_paginas = $CanvasLayer/Control_Painel_Livro/MarginContainer
+@onready var tela_atual = $CanvasLayer/Control_Painel_Livro/MarginContainer/Control_Glossario
 
 func _ready():
 	Global.moedas_alteradas.connect(atualizar_ui_moedas)
@@ -19,6 +22,7 @@ func _ready():
 	
 	atualizar_ui_moedas(Global.moedas)
 	atualizar_ui_prestigio(Global.prestigio)
+	trocar_de_tela('Glossário')
 
 # Esta função será chamada pelo botão do livro (via sinal)
 func _on_texture_button_livro_pressed():
@@ -63,7 +67,7 @@ func _on_button_receitas_pressed():
 	pass
 
 func _on_button_loja_pressed():
-	pass
+	trocar_de_tela("Loja")
 
 func _on_button_2_sair_pressed():
 	_fechar_livro()
@@ -74,3 +78,31 @@ func _on_button_3_tutoriais_pressed():
 
 func _on_button_configurações_pressed():
 	pass# Replace with function body.
+
+func trocar_de_tela(nome_alvo: String):
+	# 1. Monta o nome completo do nó que estamos procurando (ex: "Control_Loja")
+	var nome_completo_no = "Control_" + nome_alvo
+	
+	# 2. Tenta encontrar esse nó dentro do container
+	var tela_alvo = container_paginas.get_node_or_null(nome_completo_no)
+	
+	if tela_alvo:
+		# 3. Desativa a tela que estava aberta
+		if tela_atual:
+			tela_atual.visible = false
+		
+		# 4. Ativa a nova tela e atualiza a referência da 'tela_atual'
+		tela_alvo.visible = true
+		tela_atual = tela_alvo
+		
+		print("Troca concluída para: ", nome_completo_no)
+	else:
+		push_error("Erro: A tela " + nome_completo_no + " não foi encontrada no container!")
+
+
+func _on_button_voltar_pressed():
+	trocar_de_tela('Glossário')
+
+
+func _on_button_comprar_pressed():
+	pass # Replace with function body.
