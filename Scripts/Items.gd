ac514e05a7aa0bf7076511b91674bfd558076c54
Ecaf-Dev <eldestinegames@gmail.com>
2026-02-15 17:20:48 -0300

    Creado o dicionario items

---
+++ Scripts/Items.gd
@@ -0,0 +1,27 @@
+extends Node
+
+
+# Called when the node enters the scene tree for the first time.
+var itens = {
+	"flor_da_vida": {
+		"nome": "Flor Da Vida",
+		"descricao": "Uma flor vibrante que pulsa com energia vital.",
+		"valor_compra": 2,
+		"valor_venda": 0,
+		"icon": ""
+	},
+	"po_magico": {
+		"nome": "Flor Magica",
+		"descricao": "Uma flor pulsando magia",
+		"valor_compra": 2,
+		"valor_venda": 0,
+		"icon": ""
+	},
+	"frasco_vazio": {
+		"nome": "Frasco de Vidro",
+		"descricao": "Um frasco limpo pronto para receber poções.",
+		"valor_compra": 2,
+		"valor_venda": 0,
+		"icon": ""
+	}
+}
