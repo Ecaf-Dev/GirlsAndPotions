51ae061dd9e0f79d8cc8278264bbdda6508d7a1c
Ecaf-Dev <eldestinegames@gmail.com>
2026-04-05 12:23:28 -0300

    Criado a global mobilias

---
+++ GirlsAndPotions/Scripts/Mobilias.gd
@@ -0,0 +1,38 @@
+extends Node
+
+var mobilia = {
+	"Balcao": {
+		"nome": "Balcão",
+		"descricao": "Balcao de uma loja, gera pedidos a cada dia",
+		"minha_scala": 1,
+		"particularidades" : {
+			"nome_fase": "Fase 1",
+			"max_Pedidos": 3,
+			}
+	},
+	"Caldeirao" : {
+		"nome": "Caldeirão",
+		"descricao": "Um caldeirão que todo iniciante da alquimia precisa, cuidado ele explode!",
+		"minha_scala": 1,
+		"particularidades": {
+			"max_slots": 2,
+			"mobilia_automatica": true
+		}
+	},
+	"Tabua De Corte":{
+		"nome": "Tabua De Corte",
+		"descricao": "Uma tabua para cortar ad diversas criaturas e plantas",
+		"minha_scala": 1,
+		"particularidades": {
+			"max_slots": 1,
+			"mobilia_automatica": false
+		}
+	},
+	"Caixa De Correio" : {
+		"nome": "Caixa De Correio",
+		"descricao": "Uma caixa de correio, onde você ira receber suas compras!",
+		"minha_scala": 1
+	}
+		
+	
+}
